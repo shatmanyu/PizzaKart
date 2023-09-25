ee85353ca3121f2f9de575a37d61b307f20c50eb
shatmanyu_04 <shatmanyu.gupta@magicedtech.com>
2023-09-25 15:50:00 +0530

    push-to-remote

---
+++ src/ui/AppLayout.js
@@ -0,0 +1,27 @@
+import Header from './Header.js';
+import Loader from './Loader.js';
+import CartOverview from '../features/cart/CartOverview.js';
+import { Outlet, useNavigation } from 'react-router-dom';
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+
+      <Header />
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
